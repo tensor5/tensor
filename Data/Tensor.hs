86ca21d9f5c1999d065969d3e82d642b85b4346f
Nicola Squartini <tensor5@gmail.com>
2012-01-04 19:56:59 +0000

    Added class DirectSummable.

--- Data/Tensor.hs
+++ Data/Tensor.hs
@@ -65,6 +65,28 @@ type RowVector n a = Tensor (One :|: (n :|: End)) a
 
 type Matrix m n a = Tensor (m :|: (n :|: End)) a
 
+
+class DirectSummable n t1 t2 e | t1 → e, t2 → e where
+    type DirectSum n t1 t2
+    cat ∷ n → t1 → t2 → DirectSum n t1 t2
+
+instance (HNat2Integral n, MultiIndex i, MultiIndex j, MultiIndexConcat n i j)
+    ⇒ DirectSummable n (Tensor i e) (Tensor j e) e where
+        type DirectSum n (Tensor i e) (Tensor j e) = (Tensor (Concat n i j) e)
+        cat n (Tensor d x) (Tensor d' y) = Tensor ((take i d) ++ e'') (V.generate l g)
+            where l = foldl1 (*) ((take i d) ++ e'')
+                  e = drop i d
+                  e' = drop i d'
+                  e'' = ((d !! i) + (d' !! i)) : (drop (i+1) d)
+                  m = foldl1 (*) e
+                  m' = foldl1 (*) e'
+                  m'' = foldl1 (*) e''
+                  g k = if rem k m'' < m
+                        then x V.! ((quot k m'')*m + (rem k m''))
+                        else y V.! ((quot k m'')*m' + (rem k m'') - m)
+                  i = hNat2Integral n
+
+
 class (Num e) => VectorSpace e v | v -> e where
     zero ∷ v
     (*.) ∷ e -> v -> v
