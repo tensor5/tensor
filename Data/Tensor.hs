9cea22db72ca1c826b27be3bf385d5ef4c3a5045
Nicola Squartini <tensor5@gmail.com>
2012-01-15 20:56:41 +0000

    Code reorganization.
    1. Numeric.Blas.Vector has been replaced by
    Data.Tensor.LinearAlgebra and its submodules.
    
    2. All classes related to numeric operations have been moved from
    Data.Tensor to Data.Tensor.LinearAlgebra.

--- Data/Tensor.hs
+++ Data/Tensor.hs
@@ -26,66 +26,7 @@ class DirectSummable n t1 t2 e | t1 → e, t2 → e where
     cat ∷ n → t1 → t2 → DirectSum n t1 t2
 
 
-class Num e => VectorSpace e v | v -> e where
-    zero ∷ v
-    (*.) ∷ e -> v -> v
-    (.+.) ∷ v -> v -> v
---    dimension ∷ v -> Integer
-
-class (MultiIndex i) => Product e i t1 t2 t3 | t1 -> e, t2 -> e, i t1 t2 -> e where
-    prod ∷ i -> t1 -> t2 -> t3
-
-
-class MatrixProduct e t1 t2 t3 | t1 t2 -> t3, t1 -> t2, t2 -> e where
-    (.*.) ∷ t1 -> t2 -> t3
-
-
-class TensorProduct e t1 t2 t3 | t1 -> e, t2 -> e, t1 t2 -> t3 where
-    (⊗) ∷ t1 -> t2 -> t3
-
-
 class Transpose t1 t2 | t1 -> t2 where
     transpose ∷ t1 -> t2
 
 
-class DotProduct e a b c | a b -> c, a -> e, b -> e where
-    dot ∷ a -> b -> c
-
-
-class (Num e, Ordinal i, Ordinal j) => RMatrix e i j t
-                                     | t -> e, t -> i, t -> j where
-    rowSwitch ∷ i -> i -> t -> t
-    rowMult ∷ i -> e -> t -> t
-    rowAdd ∷ i -> e -> i -> t -> t
-    colSwitch ∷ j -> j -> t -> t
-    colMult ∷ j -> e -> t -> t
-    colAdd ∷ j -> e -> j -> t -> t
-
-
-class (Fractional e, Ordinal i, Ordinal j) => EchelonForm e i j t
-                                            | t -> e, t -> i, t -> j where
-    rowEchelonForm ∷ t -> t
-
-
-class (Fractional e, Ordinal i, Ordinal j) => LinearSystem e i j t1 t2
-                              | t1 -> e, t1 -> i, t1 -> j, e -> t1, e -> i where
-    solveLinSystem ∷ t1 -> t2 -> (t1,t2)
-
-
-class (Fractional e, Ordinal i) => SquareMatrix e i t | t -> e, t -> i where
-    unit ∷ t
-    inverse ∷ t -> Maybe t
-    tr ∷ t -> e
-    -- | Computes the coefficient of the polynomial p(z)=det(A+zI)
-    -- using the method of /closed ordered walks/ (/clow/) illustrated
-    -- in the paper of G. Rote
-    -- <http://page.mi.fu-berlin.de/rote/Papers/pdf/Division-free+algorithms.pdf>.
-    -- The number of operations for the whole process is O(n^4), where
-    -- n is the number of rows of the matrix. The first coefficient is
-    -- the known term and equals the determinant, while the last one
-    -- is the coefficient of z^(n-1) and equals the trace. The
-    -- coefficient of z^n equals 1 and is not included in the
-    -- resulting list. The k-th coefficient is the sum of all
-    -- principal minors of order n-k+1.
-    charPoly :: t -> [e]
-    det :: t -> e
