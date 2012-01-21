f121a41d454ddf7256ce86a57b41f7640216df17
Nicola Squartini <tensor5@gmail.com>
2012-01-21 10:30:15 +0000

    Removed leftover comments.

--- Data/Tensor.hs
+++ Data/Tensor.hs
@@ -20,31 +20,6 @@ class Tensor t where
     replicate :: Elem t -> t
     replicate e = generate (\_ -> e)
 
-{-Alas, GHC 7.0 still cannot handle equality superclasses
-rowSwitch ∷ (Ordinal i, Ordinal j ,MultiIndexable t, (Index t) ~ (i :|: (j :|: Nil)))
-              => i -> i -> t -> t
-rowSwitch _ = undefined
-
-rowMult ∷ (Ordinal i, Ordinal j ,MultiIndexable t, (Index t) ~ (i :|: (j :|: Nil)), (Elem t) ~ e, Num e)
-            => i -> e -> t -> t
-rowMult _ = undefined
-
-rowAdd ∷ (Ordinal i, Ordinal j ,MultiIndexable t, (Index t) ~ (i :|: (j :|: Nil)), (Elem t) ~ e, Num e)
-           => i -> e -> i -> t -> t
-rowAdd _ = undefined
-
-colSwitch ∷ (Ordinal i, Ordinal j ,MultiIndexable t, (Index t) ~ (i :|: (j :|: Nil)))
-              => j -> j -> t -> t
-colSwitch _ = undefined
-
-colMult ∷ (Ordinal i, Ordinal j ,MultiIndexable t, (Index t) ~ (i :|: (j :|: Nil)), (Elem t) ~ e, Num e)
-        => j -> e -> t -> t
-colMult _ = undefined
-
-colAdd ∷ (Ordinal i, Ordinal j ,MultiIndexable t, (Index t) ~ (i :|: (j :|: Nil)), (Elem t) ~ e, Num e)
-           => j -> e -> j -> t -> t
-colAdd _ = undefined
--}
 
 class DirectSummable n t1 t2 where
     type DirectSum n t1 t2
